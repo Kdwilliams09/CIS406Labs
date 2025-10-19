ecb948ebf5de86a0640962d9e4e849d2390adc43
ucctn <chetan.singhal@ucertify.com>
2025-10-19 07:38:29 -0600

    part1

---
+++ studentRegistration/src/studentRegistration/StudentRegistration.java
@@ -0,0 +1,12 @@
+//Kayla Williams CIS406 Student Registration Lab//
+package studentRegistration;
+
+public class StudentRegistration {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(StudentRegistration);
+		
+		//promt for user input//
+		System.out.print( "Enter First Name: ");
+		Scanner
