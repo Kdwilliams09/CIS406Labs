a94297f878c93c8950194cecaf959b601a55296e
ucctn <chetan.singhal@ucertify.com>
2025-10-19 11:14:08 -0600

    final draft

--- vactiondiary1/src/vactiondiary1/Vacationdiary1.java
+++ vactiondiary1/src/vactiondiary1/Vacationdiary1.java
@@ -7,10 +7,11 @@ public class Vacationdiary1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("Vaction Diary \n");
+		System.out.print("Vaction Diary");
+		System.out.println();
 		
 		// user input prompt//
-		System.out.print("Enter the Data Vaction started (mm/dd/yyy: ");
+		System.out.print("Enter the Data Vaction started (mm/dd/yyyy): ");
 		Scanner input = new Scanner(System.in);
 		String startDate = input.next();
 		
@@ -22,6 +23,19 @@ public class Vacationdiary1 {
 		String numDays = input.next();
 		System.out.print("Enter Mode of Travel (car, airplane, ship, train, bus): ");
 		String travelMode = input.next();
+		
+		//Display the output//
+		System.out.println();
+		System.out.println("Details of your trip: ");
+		System.out.println("Date: " + startDate);
+		System.out.println("City: " + placeCity);
+		System.out.println("Country: " + placeCountry);
+		System.out.println("Number of Days: " + numDays);
+		System.out.println("Traveled By: " + travelMode);
+			
+		
+		
+		
 		
 	}
 
