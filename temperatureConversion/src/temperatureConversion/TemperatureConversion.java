b8150392b7fcd62b1a1b8ba72d54814fe22c339b
Kdwilliams09 <139511741+Kdwilliams09@users.noreply.github.com>
2025-10-31 19:18:54 -0400

    Update TemperatureConversion.java

--- temperatureConversion/src/temperatureConversion/TemperatureConversion.java
+++ temperatureConversion/src/temperatureConversion/TemperatureConversion.java
@@ -23,8 +23,8 @@ public class TemperatureConversion {
 		degreesC = input.nextDouble();
 				
 		//covert fahrenheit to celsius //
-		double degreeF = (degreesC * 9.0/5) + 32;
-		System.out.println("Degrees in Fahrenheit: " + (int) degreeF);
+		degreesF = (degreesC * 9.0/5) + 32;
+		System.out.println("Degrees in Fahrenheit: " + (int) degreesF);
 		
 		
 		
