931c178f7e61627a4fd4c0194b3a6d9209fa5241
ucctn <chetan.singhal@ucertify.com>
2025-10-31 16:59:27 -0600

    first try

--- temperatureConversion/src/temperatureConversion/TemperatureConversion.java
+++ temperatureConversion/src/temperatureConversion/TemperatureConversion.java
@@ -1,9 +1,39 @@
 package temperatureConversion;
 
+import java.util.Scanner;
+
 public class TemperatureConversion {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Welcome to the Temperature Coverter");
+		
+		//create scanner class object for reading consul input//
+		Scanner input = new Scanner(System.in);
+		
+		//get temp in Fahrenheit //
+		System.out.print("\nEnter Degrees in Fahrenheit: ");
+		double degreesF = input.nextDouble();
+		
+		//covert fahrenheit to celsius //
+		double degreesC = (degreesF -32) * 5.0/9;
+		System.out.println("Degrees in Celsius: " + (int) degreesC);
+		
+		//get temp in Celsius //
+		System.out.print("\nEnter Degrees in Celsius: ");
+		degreesC = input.nextDouble();
+				
+		//covert fahrenheit to celsius //
+		double degreeF = (degreesC * 9.0/5) + 32;
+		System.out.println("Degrees in Fahrenheit: " + (int) degreeF);
+		
+		
+		
+		
+		
+		//exit and close Scanner//
+		System.out.println("\n\nBye!!");
+		input.close();
+		
 
 	}
 
