d8f3c4bd8711555f73418975064bd4fd1a7f773a
ucctn <chetan.singhal@ucertify.com>
2025-10-31 18:20:51 -0600

    push2

--- travelTimeConverter/src/travelTimeConverter/TravelTimeConverter.java
+++ travelTimeConverter/src/travelTimeConverter/TravelTimeConverter.java
@@ -1,9 +1,38 @@
 package travelTimeConverter;
 
+import java.util.Scanner;
+
 public class TravelTimeConverter {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("Welcome to the Travel Time Calculator");
+		
+		//create scanner class object for reading consul input//
+		Scanner input = new Scanner(System.in);
+		
+		//get miles  //
+		System.out.print("\nEnter Miles: ");
+		double miles= input.nextDouble();
+		
+		//get miles per hours //
+		System.out.print("\nEnter Miles Per Hour: ");
+		double mph= input.nextDouble();
+		
+		//calculate trave time hrs and min output//
+		double hours = miles / mph;
+		System.out.println("Estimated Travel Time");
+		System.out.println("---------------------");
+		System.out.println("Hours: " + (int) hours);
+		
+		double minutes = ((hours - (int)hours) * 60);
+		System.out.println("Minutes: " + (int) minutes);
+		
+		
+		//exit and close Scanner//
+		System.out.println("\nBye!!");
+		input.close();
+				
 
 	}
 
