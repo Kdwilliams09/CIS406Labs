db5d2fe5009030ad2ea1f8c0c9353820f6aff4da
ucctn <chetan.singhal@ucertify.com>
2025-11-07 16:39:10 -0700

    1st bit

---
+++ orderEntryPhase2/src/orderEntryPhase2/OrderEntryPhase2.java
@@ -0,0 +1,31 @@
+//Kayla Williams CIS 406
+//Order Entry Phase 2
+
+package orderEntryPhase2;
+
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
+public class OrderEntryPhase2 {
+
+	public static void main(String[] args) {
+		
+		// Instance Variables//
+		String itemNumber;
+		String itemDescription;
+		double itemPrice;
+		int itemQuantity;
+		double grossAmount;
+		double itemTax;
+		double taxAmount;
+		double totalDiscount;
+		double discountamount;
+		double netAmount;
+		
+		//Input Data//
+		Scanner input = new Scanner(System.in);
+		
+
+	}
+
+}
