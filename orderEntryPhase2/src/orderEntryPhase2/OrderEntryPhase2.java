cc636727c5a2b4243c7d677a06ba3011adf7f0e0
ucctn <chetan.singhal@ucertify.com>
2025-11-07 17:09:08 -0700

    2nd bit

--- orderEntryPhase2/src/orderEntryPhase2/OrderEntryPhase2.java
+++ orderEntryPhase2/src/orderEntryPhase2/OrderEntryPhase2.java
@@ -25,6 +25,47 @@ public class OrderEntryPhase2 {
 		//Input Data//
 		Scanner input = new Scanner(System.in);
 		
+		try {
+			System.out.print("Enter Item Number: ");
+			itemNumber = input.nextLine();
+			
+			System.out.print("Enter Item Description: ");
+			itemDescription = input.nextLine();
+					
+			System.out.print("Enter Item Price: $");
+			itemPrice = input.nextDouble();
+					
+			System.out.print("Enter Quantity Ordered: ");
+			itemQuantity = input.nextInt();
+			
+			System.out.print("Enter The Total Tax for the item (decimal): ");
+			itemTax = input.nextDouble();
+					
+			System.out.print("Enter the Total Discount for the item (decimal): $");
+			totalDiscount = input.nextDouble();
+					
+			System.out.print("Enter the Net Amount for the item (decimal): $");
+			netAmount = input.nextDouble();
+		}
+		catch(InputMismatchException e){
+			System.out.println("Input Format Error!");
+			e.printStackTrace();
+		}
+		catch(Exception e) {
+			System.out.println("Another Input Errorr!");
+		}
+		
+		// Calculations //
+		
+		
+		
+		
+		
+		
+		
+		
+		//Finished//
+		input.close();
 
 	}
 
